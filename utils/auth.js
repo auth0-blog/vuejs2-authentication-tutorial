5e16faffb2ffbeeb4698acb057f80ee6bf925309
Kyle Bradshaw <kylebradshaw@users.noreply.github.com>
2017-04-21 10:42:11 -0400

    more to fix

--- utils/auth.js
+++ utils/auth.js
@@ -10,7 +10,7 @@ var router = new Router({
 });
 
 export function login() {
-  window.location.href = `https://{YOUR-AUTH0-DOMAIN}.auth0.com/authorize?scope=full_access&audience=http://startupbattle.com&response_type=id_token%20token&client_id=m6WOyitl2hLmn2Afs7oiZKnJHDoZQnSY&redirect_uri=http://localhost:8080/callback&nonce=${generateNonce()}`;
+  window.location.href = `https://{YOUR-AUTH0-DOMAIN}.auth0.com/authorize?scope=full_access&audience={YOUR-API-IDENTIFIER}&response_type=id_token%20token&client_id={YOUR-AUTH0-CLIENT-ID}&redirect_uri={YOUR-CALLBACK-URL}&nonce=`;
 }
 
 export function logout() {
